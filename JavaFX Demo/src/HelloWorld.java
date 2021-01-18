2eb7b37c14dcd44e958e6a5645692b0677104162
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-18 17:28:49 -0500

    Learned scene switching

--- JavaFX Demo/src/HelloWorld.java
+++ JavaFX Demo/src/HelloWorld.java
@@ -1,41 +1,43 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
  
 public class HelloWorld extends Application {
-	Button button;
+
+	Stage window;
+	Scene scene1, scene2;
+	
     public static void main(String[] args) {
         launch(args);
     }
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("My JavaFX Program");
-		button = new Button("Click me!");
+		window = primaryStage;
 		
-//		button.setOnAction(new EventHandler<ActionEvent>() {
-//			@Override
-//			public void handle(ActionEvent event) {
-//				System.out.println("You clicked the button!");
-//			}
-//		});
+		Label label1 = new Label("Welcome to the first scene!");
+		Button button1 = new Button("Go to scene 2");
+		button1.setOnAction(e -> window.setScene(scene2));
 		
-		button.setOnAction(e -> System.out.println("You clicked the button!"));
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button1);
+		scene1 = new Scene(layout1, 200, 200);
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		Button button2 = new Button("Go back to scene 1");
+		button2.setOnAction(e -> window.setScene(scene1));
 		
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(button2);
+		scene2 = new Scene(layout2, 600, 300);
+		
+		window.setScene(scene1);
+		window.setTitle("Multiple Scene demo");
+		window.show();
 	}
 	
 }
