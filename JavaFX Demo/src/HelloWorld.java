3607ad5135d4c048b5f39f4bd15cd573d6790951
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-18 17:40:32 -0500

    Learned alert boxes

--- JavaFX Demo/src/HelloWorld.java
+++ JavaFX Demo/src/HelloWorld.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class HelloWorld extends Application {
 
 	Stage window;
-	Scene scene1, scene2;
+	Button button;
 	
     public static void main(String[] args) {
         launch(args);
@@ -19,25 +19,16 @@ public class HelloWorld extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
+		window.setTitle("Alert Box Demo");
 		
-		Label label1 = new Label("Welcome to the first scene!");
-		Button button1 = new Button("Go to scene 2");
-		button1.setOnAction(e -> window.setScene(scene2));
+		button = new Button("Click!");
+		button.setOnAction(e -> AlertBox.display("Alert", "This is an alert!"));
 		
-		VBox layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1, button1);
-		scene1 = new Scene(layout1, 200, 200);
-		
-		Button button2 = new Button("Go back to scene 1");
-		button2.setOnAction(e -> window.setScene(scene1));
-		
-		StackPane layout2 = new StackPane();
-		layout2.getChildren().add(button2);
-		scene2 = new Scene(layout2, 600, 300);
-		
-		window.setScene(scene1);
-		window.setTitle("Multiple Scene demo");
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
 		window.show();
-	}
+	} //
 	
 }
