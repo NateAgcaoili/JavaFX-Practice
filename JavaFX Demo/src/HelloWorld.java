ed2a75c6c07a16c818bfcd7f2511de4b3539fc68
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-01-18 16:08:56 -0500

    Created simple JavaFX app

--- JavaFX Demo/src/HelloWorld.java
+++ JavaFX Demo/src/HelloWorld.java
@@ -1,33 +1,29 @@
- 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
  
 public class HelloWorld extends Application {
+	
     public static void main(String[] args) {
         launch(args);
     }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("My JavaFX Program");
+		Button button = new Button("Click me!");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
