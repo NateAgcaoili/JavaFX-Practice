304d7ec8db787459f22171bf88aaf1344426aee4
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-02-04 14:59:49 -0500

    Create MiscPractice.java

---
+++ JavaFX Demo/src/MiscPractice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MiscPractice {
+	public static void main(String[] args) {
+		/*int randInt = (int)(Math.random() * 700);
+		int statusNum = randInt / 100;
+//		double assignedRaisePercentage = 0;
+		double[] raisePercentages = {0, 3.7, 4.2, 5.7, 6.1, 7.3, 8.2};
+		double assignedRaisePercentage = raisePercentages[statusNum];
+//		if (statusNum == 1) {
+//			assignedRaisePercentage = 3.7;
+//		} else if (statusNum == 2) {
+//			assignedRaisePercentage = 4.2;
+//		} else if (statusNum == 3) {
+//			assignedRaisePercentage = 5.7;
+//		} else if (statusNum == 4) {
+//			assignedRaisePercentage = 6.1;
+//		} else if (statusNum == 5) {
+//			assignedRaisePercentage = 7.3;
+//		} else {
+//			assignedRaisePercentage = 8.2;
+//		}
+		System.out.println("Status Number: " + statusNum + "\n" + "Assigned Raise Percentage: " + assignedRaisePercentage);*/
+		Scanner sc = new Scanner(System.in);
+		double num = sc.nextDouble();
+		System.out.println(num);
+		
+	}
+}
